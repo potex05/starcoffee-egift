d651698952fdee1025c4a49b8ed87ebe9e4e4b1d
potex05 <potetopoteto05@gmail.com>
2025-05-16 21:47:59 +0900

    Update script.js

--- script.js
+++ script.js
@@ -11,28 +11,12 @@
     }
   }
 
-    function showModal() {
-      const modal = document.getElementById("codeModal");
-      modal.classList.add("show");
-    }
-
-    function closeModal() {
-      const modal = document.getElementById("codeModal");
-      modal.classList.remove("show");
-
-      // opacityが消えるまで少し待ってから非表示にする（display: none）
-  setTimeout(() => {
-    if (!modal.classList.contains("show")) {
-      modal.style.display = "none";
-    }
-  }, 300);
+    function openModal() {
+  const modal = document.querySelector('.modal');
+  modal.classList.add('show');
 }
 
-// 表示時に display:flex を強制してからアニメーション発動
-const modal = document.getElementById("codeModal");
-const observer = new MutationObserver(() => {
-  if (modal.classList.contains("show")) {
-    modal.style.display = "flex";
-  }
-});
-observer.observe(modal, { attributes: true, attributeFilter: ['class'] });
+function closeModal() {
+  const modal = document.querySelector('.modal');
+  modal.classList.remove('show');
+}
